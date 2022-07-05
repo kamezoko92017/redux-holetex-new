06a55d31f158559142a37b719fee88b8162bcd18
albertbrasi <albertbrasi001@gmail.com>
2022-07-05 23:06:07 +0700

    Sử dụng combineReducers

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,12 +1,18 @@
+import { combineReducers } from "redux"
 import filtersReducer from "../components/Filters/FiltersSlice"
 import todoListReducer from "../components/Todo/TodosSlice"
 
 //Reducer luôn nhận vào 2 tham số là state và action
-const rootReducer = (state = {}, action) => {
-    return {
-        filters: filtersReducer(state.filters, action),
-        todoList: todoListReducer(state.todoList, action)
-    }
-}
+// const rootReducer = (state = {}, action) => {
+//     return {
+//         filters: filtersReducer(state.filters, action),
+//         todoList: todoListReducer(state.todoList, action)
+//     }
+// }
+
+const rootReducer = combineReducers({
+    filters: filtersReducer,
+    todoList: todoListReducer
+})
 
 export default rootReducer
